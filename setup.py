a7c0d7f76b51e49336087de2786d929d3b43766d
Andela Developer <johnboscoohia@gmail.com>
2018-03-20 12:29:59 +0100

    add a readme.rst file

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, find_packages
 
-try:
-    from pypandoc import convert_file
-    read_md_file = lambda f: convert_file(f, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md_file = lambda f: open(f, 'r').read()
-
 setup(name='ravepaypysdk',
       version='0.1',
       description='A Python library to consume the RavePay API',
-      long_description=read_md_file('README.md'),
+      long_description=open('README.rst', 'r').read(),
       url='https://github.com/johnchuks/RavePay-SDK-Python',
       author='Johnbosco Ohia',
       author_email='johnboscoohia@gmail.com',
